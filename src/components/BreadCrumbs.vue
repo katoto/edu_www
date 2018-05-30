78e933af58afd88cc833c7a41a660974325582cb
guowf <380482989@qq.com>
2018-05-30 14:15:06 +0800

    'd'

---
+++ src/components/BreadCrumbs.vue
@@ -0,0 +1,53 @@
+<template>
+    <!--面包屑-->
+    <div class="b-nav">
+        <a href="./index.html">Home</a>
+        <a href="javascript:;" class="now">terms</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {}
+        },
+        watch: {},
+        methods: {},
+        computed: {},
+        mounted(){
+
+        }
+    }
+</script>
+<style scoped lang="less">
+    //面包屑
+    .b-nav{
+        height:50px;
+        line-height:50px;
+        a{
+            position: relative;
+            display: block;
+            height:100%;
+            float: left;
+            text-transform: capitalize;
+        }
+        a+a{
+            padding-left:4px;
+            margin-left:7px;
+            text-indent: 7px;
+            &::before{
+                content: '';
+                display: block;
+                position: absolute;
+                width: 4px;
+                height: 8px;
+                left:0;
+                top:21.5px;
+                background-image: url("../assets/slice/b-nav.png");
+            }
+        }
+        .now{
+            color: #778ca3;
+        }
+    }
+</style>
--- src/pages/App.vue
+++ src/pages/App.vue
@@ -13,8 +13,15 @@
 <style lang="less">
     @import "../styles/lib-reset.css";
     @import "../styles/lib-public.less";
-    .test{
-        .transition();
+    @import "../styles/lib-font.less";
+    #app{
+        position: relative;
+        min-width: 1190px;
+        background: #eef1f9;
+        line-height: 20px;
+        font-family: sans-r;
+        font-size: 14px;
+        color: #263648;
     }
 
 </style>
