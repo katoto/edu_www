ffd4bedb9ca611f3650fb3907dbcf9c2b0cabb90
guowf <380482989@qq.com>
2018-06-04 09:51:54 +0800

    'd'

--- src/router/index.js
+++ src/router/index.js
@@ -52,32 +52,32 @@ export default new Router({
             component: account,
             children: [
                 {
-                    path: "/account/general",
+                    path: "general",
                     name: "General",
                     component: General
                 },
                 {
-                    path: "/account/deposit",
+                    path: "deposit",
                     name: "Deposit",
                     component: Deposit
                 },
                 {
-                    path: "/account/myBets",
+                    path: "myBets",
                     name: "MyBets",
                     component: MyBets
                 },
                 {
-                    path: "/account/myTransactions",
+                    path: "myTransactions",
                     name: "MyTransactions",
                     component: MyTransactions
                 },
                 {
-                    path: "/account/withdraw",
+                    path: "withdraw",
                     name: "Withdraw",
                     component: Withdraw
                 },
                 {
-                    path: "/account/*",
+                    path: "/",
                     name: "General",
                     component: General
                 }
