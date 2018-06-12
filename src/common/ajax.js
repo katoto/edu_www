c89e3d2dbb724ca0cf1d87a5e2e63b443b5fbdc9
Richole <richole.yu@qq.com>
2018-06-12 15:33:00 +0800

    更新公共ajax组件

--- src/common/ajax.js
+++ src/common/ajax.js
@@ -7,50 +7,45 @@ const options = {}
 
 // todo
 if (process.env.NODE_ENV === 'production') {
-	if (window.location.protocol === 'http:') {
-		options.baseURL = window.location.protocol + '//www.coinslot.com/api'
-	} else {
-		options.baseURL = window.location.protocol + '//www.coinslot.com/api'
-	}
+    if (window.location.protocol === 'http:') {
+        options.baseURL = window.location.protocol + '//www.coinslot.com/api'
+    } else {
+        options.baseURL = window.location.protocol + '//www.coinslot.com/api'
+    }
 } else if (process.env.NODE_ENV === 'preRelease') {
-	// 线下167
-	options.baseURL = 'http://10.0.1.41:6899'
-	// options.baseURL = 'http://crazybet.choopaoo.com:8090'
+    // 线下167
+    options.baseURL = 'http://10.0.1.41:6899'
+    // options.baseURL = 'http://crazybet.choopaoo.com:8090'
 } else {
-	// options.baseURL = 'https://www.coinslot.com/api'
-    options.baseURL = 'http://10.0.1.167:8095';  // 8095
+    // options.baseURL = 'https://www.coinslot.com/api'
+    options.baseURL = 'http://10.0.1.167:8095' // 8095
 }
 
 const _axios = axios.create(options)
 
 const ajax = function (url, config = {ignore: true}) {
-	return _axios.get(url, config).then((response) => {
-		if (response.status === 200) {
-			return response.data
-		} else if (config.ignore) {
-			return {data: {}}
-		}
-		throw new Error(response.message)
-	})
+    return _axios.get(url, config).then((response) => {
+        if (response.status === 200) {
+            return response.data
+        } else if (config.ignore) {
+            return {data: {}}
+        }
+        throw new Error(response.message)
+    })
 }
-ajax.get = function (url, config = {ignore: true}) {
-	return _axios.get(url, config).then((response) => {
-		if (response.status === 200) {
-			return response.data
-		} else if (config.ignore) {
-			return {data: {}}
-		}
-		throw new Error(response.message)
-	})
+ajax.get = function (url, params) {
+    let config = {
+        params,
+        ignore: true
+    }
+    return _axios.get(url, config).then((response) => {
+        if (response.status === 200) {
+            return response.data
+        } else if (config.ignore) {
+            return {data: {}}
+        }
+        throw new Error(response.message)
+    })
 }
-ajax.post = function (url, param, config = {ignore: true}) {
-	return _axios.post(url, param, config).then((response) => {
-		if (response.status === 200) {
-			return response.data
-		} else if (config.ignore) {
-			return {data: {}}
-		}
-		throw new Error(response.message)
-	})
-}
+
 export default ajax
