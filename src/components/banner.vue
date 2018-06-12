adfd0c10da6973e7e0df5163465bd539a31d81e6
guowf <380482989@qq.com>
2018-06-12 17:36:58 +0800

    'd'

---
+++ src/components/banner.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="banner"  v-if="banner">
+        <a href="https://www.coinslot.com/coinslot/html/worldCup.html" class="banner-main">
+            <img src="../assets/img/banner.jpg" alt="">
+        </a>
+        <a href="javascript:;" class="banner-close" @click="closeBanner"></a>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                banner:true
+            }
+        },
+        watch: {},
+        methods: {
+            closeBanner(){
+               this.banner = false
+            }
+        },
+        computed: {},
+        mounted(){
+
+        }
+    }
+</script>
+<style scoped lang="less">
+    .banner{
+        position: relative;
+        height:90px;
+        .banner-main{
+            display: block;
+            position: absolute;
+            left:50%;
+            margin-left:-960px;
+            height:90px;
+        }
+        .banner-close{
+            display: block;
+            position: absolute;
+            left:50%;
+            margin-left:580px;
+            top:7px;
+            width:20px;
+            height:20px;
+            overflow: hidden;
+            background: rgba(255,255,255,0.2);
+            border-radius: 50%;
+            &::after{
+                content: '';
+                display: block;
+                background-image: url("../assets/slice/close-banner.png");
+                margin:5px auto;
+                width: 10px;
+                height: 10px;
+            }
+            &:hover{
+                transform: rotate(90deg);
+            }
+        }
+    }
+</style>
