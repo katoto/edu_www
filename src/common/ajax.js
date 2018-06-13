fb55d42e21637d3a6efead6340b38baf3a7fff09
Richole <richole.yu@qq.com>
2018-06-13 10:14:35 +0800

    更新公共ajax

--- src/common/ajax.js
+++ src/common/ajax.js
@@ -6,12 +6,13 @@ import { getCK, platform, tipsTime, src } from '~common/util'
 import { Message } from 'element-ui'
 
 function getCommonParams () {
-    return {
-        ck: getCK(),
+    let ck = getCK()
+    let params = {
         platform,
         src,
         lotid: 1
     }
+    return ck ? { ...params, ck } : params
 }
 
 const options = {}
@@ -28,7 +29,6 @@ if (process.env.NODE_ENV === 'production') {
     options.baseURL = 'http://10.0.1.41:6899'
     // options.baseURL = 'http://crazybet.choopaoo.com:8090'
 } else {
-
     // options.baseURL = 'https://www.coinslot.com/api'
     options.baseURL = 'http://10.0.1.167:8095' // 8095
 }
@@ -47,8 +47,10 @@ const ajax = function (url, config = {ignore: true}) {
 }
 ajax.get = function (url, params) {
     let config = {
-        ...getCommonParams(),
-        params,
+        params: {
+            ...getCommonParams(),
+            ...params
+        },
         ignore: true
     }
     return _axios.get(url, config)
