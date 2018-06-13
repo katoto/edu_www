39475dba145189285084d8cb518d872ae2d31473
Richole <richole.yu@qq.com>
2018-06-13 15:18:48 +0800

    代码整理

--- src/router/index.js
+++ src/router/index.js
@@ -5,93 +5,93 @@ Vue.use(Router)
 
 /* cs_1105 */
 /* cs_1105 首页 */
-const lucky = () => import ('~/pages/cs_1105/lucky')
+const lucky = () => import('~/pages/cs_1105/lucky')
 /* cs_1105 开奖页 */
-const cs_drawNum = () => import ('~/pages/cs_1105/drawNumber')
+const cs_drawNum = () => import('~/pages/cs_1105/drawNumber')
 
 /* 协议 */
-const cs_protocol_policy = () => import ('~/pages/cs_protocol/policy')
-const cs_protocol_terms = () => import ('~/pages/cs_protocol/terms')
+const cs_protocol_policy = () => import('~/pages/cs_protocol/policy')
+const cs_protocol_terms = () => import('~/pages/cs_protocol/terms')
 
-/*个人中心*/
-const account = () => import ("~/pages/cs_account/account")
-const General = () => import ("~/pages/cs_account/General")
-const Deposit = () => import ("~/pages/cs_account/Deposit")
-const MyBets = () => import ("~/pages/cs_account/MyBets")
-const MyTransactions = () => import ("~/pages/cs_account/MyTransactions")
-const Withdraw = () => import ("~/pages/cs_account/Withdraw")
+/* 个人中心 */
+const account = () => import('~/pages/cs_account/account')
+const General = () => import('~/pages/cs_account/General')
+const Deposit = () => import('~/pages/cs_account/Deposit')
+const MyBets = () => import('~/pages/cs_account/MyBets')
+const MyTransactions = () => import('~/pages/cs_account/MyTransactions')
+const Withdraw = () => import('~/pages/cs_account/Withdraw')
 
 // 404
-const page_404 = () => import ('~/pages/404.vue')
+const page_404 = () => import('~/pages/404.vue')
 
 export default new Router({
-	mode: "hash",
+	mode: 'hash',
 	linkActiveClass: 'on',
 	routes: [
 		{
-			path: "/lucky",
-			name: "lucky",
+			path: '/lucky',
+			name: 'lucky',
 			component: lucky
 		},
 		{
-			path: "/terms",
-			name: "terms",
+			path: '/terms',
+			name: 'terms',
 			component: cs_protocol_terms
 		},
 		{
-			path: "/policy",
-			name: "policy",
+			path: '/policy',
+			name: 'policy',
 			component: cs_protocol_policy
 		},
 		{
-			path: "/drawNumber",
-			name: "Draw Number",
+			path: '/drawNumber',
+			name: 'Draw Number',
 			component: cs_drawNum
 		},
 		{
-			path: "/404",
+			path: '/404',
 			component: page_404,
 			hidden: true
 		},
 		{
-			path: "/account",
+			path: '/account',
 			component: account,
 			children: [
 				{
-					path: "general",
-					name: "General",
+					path: 'general',
+					name: 'General',
 					component: General
 				},
 				{
-					path: "deposit",
-					name: "Deposit",
+					path: 'deposit',
+					name: 'Deposit',
 					component: Deposit
 				},
 				{
-					path: "myBets",
-					name: "MyBets",
+					path: 'myBets',
+					name: 'MyBets',
 					component: MyBets
 				},
 				{
-					path: "myTransactions",
-					name: "MyTransactions",
+					path: 'myTransactions',
+					name: 'MyTransactions',
 					component: MyTransactions
 				},
 				{
-					path: "withdraw",
-					name: "Withdraw",
+					path: 'withdraw',
+					name: 'Withdraw',
 					component: Withdraw
 				},
 				{
-					path: "/",
-					name: "General",
+					path: '/',
+					name: 'General',
 					redirect: '/account/general'
 				}
 			]
 		},
 		{
-			path: "/*",
-			name: "lucky",
+			path: '/*',
+			name: 'lucky',
 			redirect: '/lucky'
 		}
 	]
@@ -117,5 +117,4 @@ if (location.search) {
 			}
 		}
 	}
-
 }
