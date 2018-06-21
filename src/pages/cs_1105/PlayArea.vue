bed7f74a640f249755a094d61d315ce87492b42e
Richole <richole.yu@qq.com>
2018-06-21 15:53:49 +0800

    更新11选1 选择号码优化

--- src/pages/cs_1105/PlayArea.vue
+++ src/pages/cs_1105/PlayArea.vue
@@ -202,10 +202,7 @@
                         if (this.areaMsg && this.areaMsg.pickType) {
                             if (currpickNum.length >= parseFloat(this.areaMsg.pickType)) {
                                 if (parseFloat(this.areaMsg.pickType) === 1) {
-                                    Message({
-                                        message: 'You have already chosen ' + parseFloat(this.areaMsg.pickType) + ' number',
-                                        type: 'error'
-                                    })
+                                    currpickNum = [parseFloat(dataFlag)]
                                 } else {
                                     Message({
                                         message: 'You have already chosen ' + parseFloat(this.areaMsg.pickType) + ' numbers',
