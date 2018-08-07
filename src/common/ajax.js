cf02a64a44f8c3e6bb4f1d34663c803e562421eb
xiezg <xiezg@500wan.com>
2018-08-07 18:05:28 +0800

    '87'

--- src/common/ajax.js
+++ src/common/ajax.js
@@ -43,8 +43,8 @@ if (process && process.env && process.env.NODE_ENV === 'production') {
     // websocketUrl = `${isHttp ? 'ws' : 'wss'}://192.168.30.13:7999/betblock`// 张旭ws
     // websocketUrl = `${isHttp ? 'ws' : 'wss'}://10.0.0.171:8098/betblock`// 成哥ws
     // websocketUrl = `${isHttp ? 'ws' : 'wss'}://10.0.0.130:8080/betblock'`// 建清ws
-    websocketUrl = `${isHttp ? 'ws' : 'wss'}://10.0.1.41:4444/betblock`// 线下测试环境ws
-    // websocketUrl = `${isHttp ? 'ws' : 'wss'}://10.0.0.171:7081/betblock`// 线下测试环境ws
+    // websocketUrl = `${isHttp ? 'ws' : 'wss'}://10.0.1.41:4444/betblock`// 线下测试环境ws
+    websocketUrl = `${isHttp ? 'ws' : 'wss'}://10.0.0.176:8001/betblock`// 线下测试环境ws
 } else {
     // 开发环境
     // options.baseURL = 'http://192.168.30.13:8000'// 张旭web
@@ -57,8 +57,8 @@ if (process && process.env && process.env.NODE_ENV === 'production') {
     // websocketUrl = `${isHttp ? 'ws' : 'wss'}://10.0.0.171:8098/betblock`// 成哥ws
 
     // websocketUrl = `${isHttp ? 'ws' : 'wss'}://10.0.0.130:8080/betblock'`// 建清ws
-    websocketUrl = `${isHttp ? 'ws' : 'wss'}://10.0.1.41:4444/betblock`// 线下测试环境ws
-    // websocketUrl = `${isHttp ? 'ws' : 'wss'}://10.0.0.171:7081/betblock`// 线下测试环境ws
+    // websocketUrl = `${isHttp ? 'ws' : 'wss'}://10.0.1.41:4444/betblock`// 线下测试环境ws
+    websocketUrl = `${isHttp ? 'ws' : 'wss'}://10.0.0.176:8001/betblock`// 线下测试环境ws
 }
 
 export const sockURL = websocketUrl
