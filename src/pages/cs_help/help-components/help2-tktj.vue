2f31c8e9443b4fed28ca8f07203bbb751200bf6d
guowf <380482989@qq.com>
2018-08-14 14:45:29 +0800

    '修改面包屑'

--- src/pages/cs_help/help-components/help2-tktj.vue
+++ src/pages/cs_help/help-components/help2-tktj.vue
@@ -1,34 +1,29 @@
 <template>
     <div class="">
         <h1>
-            流水说明
+            提款条件
         </h1>
         <ul>
             <li>
                 <p class="title">
-                    1. 计算公式
+                    1. 提现额度
                 </p>
                 <p class="msg">
-                    流水额 = Lucky11投注额 + 1/5 slot流水
+                    最大提现额度 = 1/10 * 平台有效投注额+ 历史提现总额
+                </p>
+                <p class="msg">
+                    （*平台有效投注额 = Lucky11游戏投注额 + 1/5 slot游戏投注额）
                 </p>
             </li>
             <li>
                 <p class="title">
-                    2. 约束条件
+                    2. 资金流水分配
                 </p>
                 <p class="msg">
-                    可提现额度 = 1/10 * 流水总额 + 历史提现总额
+                    充值金额全部进入可提现账户
                 </p>
-            </li>
-            <li>
-                <p class="title">
-                    3. 资金流水分配
-                </p>
-                <p class="cicle">
-                    · <lang>All top-up is withdrawable</lang>
-                </p>
-                <p class="cicle">
-                    · <lang>All bonus from activities on the platform is non-withdrawable</lang>
+                <p class="msg">
+                    赠送金额全部进入不可提现账户
                 </p>
             </li>
         </ul>
