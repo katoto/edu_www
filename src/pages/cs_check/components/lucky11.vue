cd953bfa3d2a838a0d545fe17d70fe289f2e60bc
Richole <richole.yu@qq.com>
2018-08-16 18:12:47 +0800

    更新验证页面

--- src/pages/cs_check/components/lucky11.vue
+++ src/pages/cs_check/components/lucky11.vue
@@ -271,6 +271,8 @@ export default {
                 }).then(res => {
                     if (res.data.detail.expect_status === '4' || res.data.detail.expect_status === '5') {
                         this.renderCheckData(res.data.detail)
+                    } else if (res.data.detail.expect_status === undefined) {
+                        return Promise.reject(new Error('no number'))
                     } else {
                         let thisStatus = 'wait'
                         this.$emit('update:status', thisStatus)
