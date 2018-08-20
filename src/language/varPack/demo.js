118184fdd2a9817be9703c7f70cc522e34b692f6
Richole <richole.yu@qq.com>
2018-08-20 16:29:08 +0800

    翻译工具新增另外一种方案

---
+++ src/language/varPack/demo.js
@@ -0,0 +1,15 @@
+let en = {
+    a1: 'this is a demo'
+}
+
+let zhCn = {
+    a1: '这是一个模板'
+}
+
+let zhTw = {
+    a1: '這是一個模板'
+}
+
+// 组件内运用直接变量访问 $store.state.langs.demo.a1
+
+export default { en, zhCn, zhTw }
