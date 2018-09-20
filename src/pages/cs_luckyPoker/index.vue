094c30e77961eb853327ef038a52d5356fb90771
guowf <380482989@qq.com>
2018-09-20 10:30:03 +0800

    '禁止图片选中'

--- src/pages/cs_luckyPoker/index.vue
+++ src/pages/cs_luckyPoker/index.vue
@@ -1052,6 +1052,11 @@ export default {
 </script>
 <style lang="less" scoped type="text/less">
     @import "../../styles/lib-mixins.less";
+    .luckyPoker{
+        div, a, img, p, span, i{
+            -webkit-user-select: none;
+        }
+    }
     .fly-coin {
         position: absolute;
         left: 0;
