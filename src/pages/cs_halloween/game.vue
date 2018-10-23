54d9bdb99f8f927f5a3fe6a8f8e42906c51fe550
Richole <richole.yu@qq.com>
2018-10-23 21:16:17 +0800

    更新万圣节

--- src/pages/cs_halloween/game.vue
+++ src/pages/cs_halloween/game.vue
@@ -1,21 +1,29 @@
 <template>
-    <div class="main" ref="game" >
+    <div class="game-main" ref="game" >
         <audio :src="musicSrc" class="poker-audio" ref="musicObj"></audio>
-        <!-- <i v-for="item in ghosts" :key="item.monster_id" class="ghost" :class="{beating: !!item.beating}" >
-            <img draggable="false" src="@/assets/img/halloween/ghost-dead.png" v-if="isDead">
-            <img draggable="false" src="@/assets/img/halloween/ghost.png">
-        </i> -->
         <i class="music-btn" @click="switchMusic" ref="musicBtn"></i>
         <i class="hammer-btn" :style="{transform: `translate(${hammerX}px,${hammerY}px)`}" v-if="!hideHammer">
             
         </i>
-        <i class="ghost-ct" :style="{transform: `translate(${ghost.x}px,${ghost.y}px) rotateY(${ghost.direction ? '0deg' : '180deg'})`}" @click="beatIt" :class="{beating: ghost.beating}">
-            <img src="@/assets/img/halloween/ghost1.png" draggable="false" class="ghost1" >
+        <i class="ghost1-ct" :style="{transform: `translate(${ghost.x}px,${ghost.y}px) rotateY(${ghost.direction ? '0deg' : '180deg'})`}" @click="beatIt" :class="{beating: ghost.beating}">
+            <img src="@/assets/img/halloween/monster01.png" draggable="false" class="ghost1" v-if="ghost.animate">
+            <img src="@/assets/img/halloween/monster02.png" draggable="false" class="ghost1" v-else>
+        </i>
+        <i class="ghost2-ct" @click="beatIt" :class="{beating: ghost.beating}">
+            <img src="@/assets/img/halloween/monster11.png" draggable="false" class="ghost2" v-if="ghost2.animate">
+            <img src="@/assets/img/halloween/monster12.png" draggable="false" class="ghost2" v-else>
+        </i>
+        <i class="ghost3-ct" @click="beatIt" :class="{beating: ghost.beating}" :style="{bottom: `${ghost3.bottom}px`}">
+            <img src="@/assets/img/halloween/monster21.png" draggable="false" class="ghost3" v-if="ghost3.animate === 1">
+            <img src="@/assets/img/halloween/monster22.png" draggable="false" class="ghost3" v-else-if="ghost3.animate === 2">
+            <img src="@/assets/img/halloween/monster23.png" draggable="false" class="ghost3" v-else-if="ghost3.animate === 3">
+            <img src="@/assets/img/halloween/monster24.png" draggable="false" class="ghost3" v-else>
         </i>
     </div>
 </template>
 
 <script>
+import { getElementAbsolutePosition } from '~common/util'
 const bgMusic = () => import('~static/audio/halloween/halloween.mp3')
 export default {
     props: {
@@ -31,7 +39,9 @@ export default {
             musicSrc: '',
             isPause: false,
             ghosts: [],
-            ghost: { x: 0, y: 0, direction: true, speed: 0.01 },
+            ghost: { x: 0, y: 0, direction: true, speed: 0.01, animate: false },
+            ghost2: { },
+            ghost3: { animate: 1, bottom: 0 },
             currentData: null,
             gameWidth: null,
             gameHeight: null,
@@ -82,45 +92,6 @@ export default {
             requestAnimFrame(this.startAnimate)
             this.renderNextFrame()
         },
-        move1 (ghost) {
-            let offsetX = 1
-            let offsetY = 0.3
-            if (ghost.x + offsetX > this.gameWidth) {
-                ghost.directionX = false
-            }
-            if (ghost.x - offsetX < 0) {
-                ghost.directionX = true
-            }
-            if (ghost.y + offsetY > this.gameHeight) {
-                ghost.directionY = false
-            }
-            if (ghost.y - offsetY < 0) {
-                ghost.directionY = true
-            }
-            ghost.x = ghost.directionX ? ghost.x + offsetX : ghost.x - offsetX
-            ghost.y = ghost.directionY ? ghost.y + offsetY : ghost.y - offsetY
-        },
-        move2 (ghost) {
-            let offsetX = 0.3
-            let offsetY = 1
-            if (ghost.x + offsetX > this.gameWidth) {
-                ghost.directionX = false
-            }
-            if (ghost.x - offsetX < 0) {
-                ghost.directionX = true
-            }
-            if (ghost.y + offsetY > this.gameHeight) {
-                ghost.directionY = false
-            }
-            if (ghost.y - offsetY < 0) {
-                ghost.directionY = true
-            }
-            ghost.x = ghost.directionX ? ghost.x + offsetX : ghost.x - offsetX
-            ghost.y = ghost.directionY ? ghost.y + offsetY : ghost.y - offsetY
-        },
-        move3 () {
-
-        },
         renderNextFrame () {
             if (this.ghost.direction) {
                 if (this.ghost.x >= (this.gameWidth + 200)) {
@@ -135,8 +106,12 @@ export default {
                 }
                 this.ghost.x -= 1.4
             }
-            this.ghost.y = Number(Math.cos(this.ghost.speed).toFixed(4)) * 50
-            this.ghost.speed += 0.01
+            let num = Number(Math.cos(this.ghost.speed).toFixed(4))
+            this.ghost.y = num * 50
+            this.ghost.animate = (num > 0)
+            this.ghost2.animate = (num > 0)
+            this.ghosts.last = (num > 0)
+            this.ghost.speed += 0.05
         },
         initGhosts (ghosts) {
             ghosts.forEach(ghost => {
@@ -150,9 +125,14 @@ export default {
 
         startGame () {
             this.startAnimate()
+            this.timer = setInterval(() => {
+                this.ghost3.animate += 1
+                if (this.ghost3.animate === 5) {
+                    this.ghost3.animate = 1
+                }
+            }, 1000)
         },
         mousemove () {
-            console.log(event)
             this.hammerX = event.offsetX
             this.hammerY = event.offsetY
         },
@@ -196,6 +176,23 @@ export default {
                     this.ghost.beating = false
                 }, 1000)
             }
+        },
+        onScroll () {
+            let footerTop
+            if (this.scene === 'lucky11') {
+                footerTop = getElementAbsolutePosition(document.getElementsByClassName('pre-numberBox')[0]).top
+            } else {
+                footerTop = getElementAbsolutePosition(document.getElementsByClassName('footer')[0]).top
+            }
+            if (footerTop) {
+                let dis = window.scrollY + window.innerHeight
+                console.log(dis, footerTop, this.ghost3.bottom)
+                if (dis > footerTop) {
+                    this.ghost3.bottom = dis - footerTop
+                } else {
+                    this.ghost3.bottom = 0
+                }
+            }
         }
     },
     mounted () {
@@ -216,16 +213,22 @@ export default {
             this.gameHeight = window.document.body.clientHeight
             this.startGame()
         })
+
+        window.addEventListener('scroll', this.onScroll)
+        setTimeout(() => {
+            this.onScroll()
+        }, 400)
     },
     destroyed () {
         this.closeGame()
+        clearInterval(this.timer)
     }
 }
 </script>
 
 
 <style lang="less" scoped>
-.main {
+.game-main {
     *{
         moz-user-select: -moz-none;
         -moz-user-select: none;
@@ -248,65 +251,69 @@ export default {
         height: 20px;
         cursor: pointer !important;
     }
-    .hammer-btn {
-        position: absolute;
-        transform: translate(0px, 0px);
-        -webkit-tap-highlight-color: transparent;
-        -webkit-touch-callout: none;
-        /* -webkit-user-select: none; */
-        -webkit-appearance: none;
-        img.beat {
-            animation: beatIt .2s ease-in-out;
+    .ghost1-ct, .ghost2-ct, .ghost3-ct {
+        cursor: url(../../assets/img/halloween/1.png), auto;
+        &.beating  {
+            cursor: url(../../assets/img/halloween/2.png), auto;
         }
     }
-    .ghost-ct {
+    .ghost1-ct {
         position: fixed;
         top: 90px;
         left: 0%;
         padding: 50px;
-        &:hover {
-            cursor: url(../../assets/img/halloween/1.png), auto;
-        }
-        &.beating :hover {
-            cursor: url(../../assets/img/halloween/2.png), auto;
-        }
     }
-    .ghost1 {
+
+    .ghost1-ct {
+        position: fixed;
+        top: 90px;
+        left: 0%;
+        padding: 50px;
+    }
+
+    .ghost2-ct {
+        position: fixed;
+        top: 0;
+        left: 100px;
+        padding: 50px;
+        animation: ghost2Down 10s infinite ease-in-out;
+    }
+
+    .ghost3-ct {
+        position: fixed;
+        bottom: 0;
+        left: 0%;
+        animation: ghost3Walk 30s infinite linear;
+    }
+
+    .ghost1, .ghost2, .ghost3 {
         width: 100px;
         // animation: ghost1Fly 40s infinite ease-in-out;
     }
 
-    @keyframes beatIt {
-        from {
-            transform: rotate(0deg)
+    @keyframes ghost2Down {
+        0%, 100% {
+            transform: translate(0, -200px);
         }
-        to {
-            transform: rotate(-50deg)
+        20%, 40%, 60%, 80% {
+            transform: translate(0, 150px);
+        }
+        10%, 30%, 50%, 70%, 90% {
+            transform: translate(0, 200px);
         }
     }
 
-    @keyframes ghost1Fly {
+    @keyframes ghost3Walk {
         0% {
             transform: translate(-200px, 0) rotateY(0deg);
         }
-        20%, 40% {
-            transform: translate(0, 0) rotateY(0deg);
-        }
-        10%, 30% {
-            transform: translate(0, 50px) rotateY(0deg);
-        }
         50% {
-            left: 100%;
-            transform: translate(200px, 50px) rotateY(0deg);
+            transform: translate(2000px, 0);
         }
-        51%, 70%, 90% {
-            transform: translate(0, 0) rotateY(180deg);
-        }
-        60%, 80% {
-            transform: translate(0, 50px) rotateY(180deg);
+        51%{
+            transform: translate(2000px, 0) rotateY(180deg);
         }
         100% {
-            left: 0%;
             transform: translate(-200px, 0) rotateY(180deg);
         }
     }
