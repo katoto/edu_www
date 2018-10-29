c876ded3f3b460d2d4273cad51e26d35295824e5
guowf <380482989@qq.com>
2018-10-29 11:57:38 +0800

    ''

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -727,6 +727,13 @@ export default {
         color: #fff;
         word-wrap: break-word;
         word-break: break-all;
+        a {
+          color: #fd9644;
+          &:hover {
+            color: currentColor;
+            filter: brightness(1.1);
+          }
+        }
       }
     }
     .chat_room_foot {
