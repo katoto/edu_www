4cb1df889baed621cfb566e0a3ff4ab9830e64cf
Richole <richole.yu@qq.com>
2018-10-29 15:17:29 +0800

    更新第ä万圣节å音效 背景等

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-
-http.createServer((request, response) => {
-    let cookieString = 'Your Cookie is: '
-    let noCookieString = 'You have no cookie'
-    response.end(
-        request.headers.cookie ? (cookieString + request.headers.cookie) : noCookieString
-    )
-}).listen(9000)
