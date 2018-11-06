982f852a5f1584d520085aae84282b10dd475eca
xiezg <xiezg@qq.com>
2018-11-06 16:22:58 +0800

    '116'

--- src/pages/ka_home/index.vue
+++ src/pages/ka_home/index.vue
@@ -5,12 +5,30 @@
             <h2>热门电影</h2>
             <div class="container">
                 <ul class="row">
-                    <li class="col-lg-2 col-md-4 col-sm-4 col-xs-6"><img src="http://pic6.iqiyipic.com/image/20180220/42/9e/v_114654279_m_601_m3_180_236.jpg" alt=""></li>
-                    <li class="col-lg-2 col-md-4 col-sm-4 col-xs-6"><img src="http://pic6.iqiyipic.com/image/20180220/42/9e/v_114654279_m_601_m3_180_236.jpg" alt=""></li>
-                    <li class="col-lg-2 col-md-4 col-sm-4 hidden-xs"><img src="http://pic6.iqiyipic.com/image/20180220/42/9e/v_114654279_m_601_m3_180_236.jpg" alt=""></li>
-                    <li class="col-lg-2 hidden-md hidden-sm hidden-xs"><img src="http://pic6.iqiyipic.com/image/20180220/42/9e/v_114654279_m_601_m3_180_236.jpg" alt=""></li>
-                    <li class="col-lg-2 hidden-md hidden-sm hidden-xs"><img src="http://pic6.iqiyipic.com/image/20180220/42/9e/v_114654279_m_601_m3_180_236.jpg" alt=""></li>
-                    <li class="col-lg-2 hidden-md hidden-sm hidden-xs"><img src="http://pic6.iqiyipic.com/image/20180220/42/9e/v_114654279_m_601_m3_180_236.jpg" alt=""></li>
+                    <li class="col-lg-2 col-md-4 col-sm-4 col-xs-6">
+                        <img src="//pic2.iqiyipic.com/image/20181031/70/7c/v_120365437_m_601_m3_260_360.jpg" alt="">
+                        <p>电影名称</p>
+                    </li>
+                    <li class="col-lg-2 col-md-4 col-sm-4 col-xs-6">
+                        <img src="http://pic6.iqiyipic.com/image/20180220/42/9e/v_114654279_m_601_m3_180_236.jpg" alt="">
+                        <p>电影名称</p>
+                    </li>
+                    <li class="col-lg-2 col-md-4 col-sm-4 hidden-xs">
+                        <img src="http://pic6.iqiyipic.com/image/20180220/42/9e/v_114654279_m_601_m3_180_236.jpg" alt="">
+                        <p>电影名称</p>
+                    </li>
+                    <li class="col-lg-2 hidden-md hidden-sm hidden-xs">
+                        <img src="http://pic6.iqiyipic.com/image/20180220/42/9e/v_114654279_m_601_m3_180_236.jpg" alt="">
+                        <p>电影名称</p>
+                    </li>
+                    <li class="col-lg-2 hidden-md hidden-sm hidden-xs">
+                        <img src="http://pic6.iqiyipic.com/image/20180220/42/9e/v_114654279_m_601_m3_180_236.jpg" alt="">
+                        <p>电影名称</p>
+                    </li>
+                    <li class="col-lg-2 hidden-md hidden-sm hidden-xs">
+                        <img src="http://pic6.iqiyipic.com/image/20180220/42/9e/v_114654279_m_601_m3_180_236.jpg" alt="">
+                        <p>电影名称</p>
+                    </li>
                 </ul>
             </div>
         </section>
@@ -47,10 +65,10 @@ export default {
 @import "../../styles/lib-media.less";
 .movieContent {
   li {
+    text-align: center;
     background-color: aqua;
     img {
-      display: inline-block;
-      width: 100%;
+      display: inline;
       height: 240px;
     }
   }
