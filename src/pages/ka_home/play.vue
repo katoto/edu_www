f7f906384956ab766c2b56ab8f0a45231bdad15b
xiezg <xiezg@qq.com>
2018-11-15 21:05:24 +0800

    '1115'

--- src/pages/ka_home/play.vue
+++ src/pages/ka_home/play.vue
@@ -2,7 +2,7 @@
     <div class="test">
         <p class="ka_tips">温馨提示：如果当前打不开，可以选择换个线路~</p>
         <div style="width:100%;padding-bottom:40px;">
-            <iframe class="iframeStyle" :src="currSrc" frameborder="0" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen">
+            <iframe class="iframeStyle" :src="currSrc8" frameborder="0" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen">
             </iframe>
         </div>
     </div>
@@ -12,6 +12,13 @@ export default {
     data () {
         return {
             currSrc: '//api.nohacks.cn/index.php?url=https://www.iqiyi.com/v_19rrfnf1gk.html#vfrm=19-9-0-1',
+            currSrc2: '//jx.biaoge.tv/index.php?url=https://www.iqiyi.com/v_19rr1tjd14.html',
+            currSrc3: '//api.zuilingxian.com/jiexi.php?url=https://www.iqiyi.com/v_19rr1tjd14.html',
+            currSrc4: '//app.baiyug.cn:2019/vip/?url=https://www.iqiyi.com/v_19rr1tjd14.html', // 不是很高清 可以看电视
+            currSrc5: '//jqaaa.com/jx.php?url=https://www.iqiyi.com/v_19rr1tjd14.html', // 不是很高清
+            currSrc6: '//vip.jlsprh.com/index.php?url=https://www.iqiyi.com/v_19rr1tjd14.html', // 高清 有他人广告
+            currSrc7: '//jiexi.92fz.cn/player/vip.php?url=https://www.iqiyi.com/v_19rr1tjd14.html', //
+            currSrc8: '//www.wq114.org/yun.php?url=https://www.iqiyi.com/v_19rr1tjd14.html', //
             consoleTime: null
         }
     },
